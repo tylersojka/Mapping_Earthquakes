2c8fd2d16832b6eaf39fbe2aa751d42458412a54
tsojka <sojkasojkasojka@gmail.com>
2020-12-05 15:20:36 -0600

    added multiple points example

--- mappint_multiple_points/static/js/logic.js
+++ mappint_multiple_points/static/js/logic.js
@@ -1,19 +1,11 @@
 console.log("im working, you fucking tit.")
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([36.1627, -86.7816], 12);
+let map = L.map('mapid').setView([36.1627, -86.7816], 4);
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-let circle = L.circle([36.1627, -86.7816], {
-    radius: 5500,
-    color: 'black',
-    outline: true,
-    fillColor: "yellow",
-    fillOpacity: .25
 
- }).addTo(map);
+
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -24,3 +16,16 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+
+// Get data from cities.js
+let cityData = cities;
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+   });

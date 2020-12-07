2e7fa35ffd426b21654d6cb834e182230c1a8531
tsojka <sojkasojkasojka@gmail.com>
2020-12-07 14:17:14 -0600

    added polygon example and started style dict

--- mapping_geojson_polygons/static/js/logic.js
+++ mapping_geojson_polygons/static/js/logic.js
@@ -15,17 +15,25 @@ let style2 = L.tileLayer('https://api.mapbox.com/styles/v1/tylersojka/ckiep8ygt3
     {maxZoom: 18,
     accessToken: API_KEY});
 
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', 
+{maxZoom: 18,
+accessToken: API_KEY});
+
+let satStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', 
+{maxZoom: 18,
+accessToken: API_KEY});
+
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Standard: style1,
-  Dark: style2
+  Streets: streets,
+  Satellite: satStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44, -0],
-  zoom: 2.5,
-  layers: [style1]
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -33,25 +41,23 @@ L.control.layers(baseMaps).addTo(map);
 
 
 // Accessing the airport GeoJSON URL
-let torontoData = "https://raw.githubusercontent.com/tylersojka/Mapping_Earthquakes/main/torontoRoutes.json";
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/tylersojka/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-// Create a style for the lines.
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
+myStyle = {
+  color: "blue",
+  fillColor: "#ffffa1",
+  weight: 1
 }
-
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
     style: myStyle,
     onEachFeature: function(feature, layer) {
       layer.bindPopup(`
-      <h2> Airline: ${feature.properties.airline} </h2>  
-      <hr>
-      <h2>Destination: ${feature.properties.dst}</h2>`);
+      <h2> Neighborhood: ${feature.properties.AREA_NAME}`);
     }
   }).addTo(map);
-  });
+});
